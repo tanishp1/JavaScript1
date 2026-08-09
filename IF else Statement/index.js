4c1be9def423b8b645d25e4222c889798c0bbd18
tanishp1 <pawartanish73@gmail.com>
2026-08-09 17:27:35 +0530

    if else statement

--- IF else Statement/index.js
+++ IF else Statement/index.js
@@ -1,21 +1,36 @@
-function getGrade(score){
-    if(score >= 90 && score <= 100){
-        return "A"
-    }
-    else if( score >= 80 && score <= 89){
-        return "B"
-    }
-    else if( score >= 70 && score <= 79){
-        return "C"
-    }
-    else if( score >= 60 && score <= 69){
-        return "D"
-    }
-    else if (score <= 59){
-        return "E"
-    }
-    else {
-        return "Invaild score";
-    }
-}
-console.log(getGrade(78))
+// function getGrade(score){
+//     if(score >= 90 && score <= 100){
+//         return "A"
+//     }
+//     else if( score >= 80 && score <= 89){
+//         return "B"
+//     }
+//     else if( score >= 70 && score <= 79){
+//         return "C"
+//     }
+//     else if( score >= 60 && score <= 69){
+//         return "D"
+//     }
+//     else if (score <= 59){
+//         return "E"
+//     }
+//     else {
+//         return "Invaild score";
+//     }
+// }
+// console.log(getGrade(78))
+
+// const light = "red"
+
+// if (light == "green"){
+//     console.log("go")
+// }
+// else if(light == "yellow"){
+//     console.log("slow")
+// }
+// else if(light == "red"){
+//     console.log("stop")
+// }
+// else{
+//     console.log("invaild light")
+// }
