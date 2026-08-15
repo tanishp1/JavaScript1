2bcf7878105dd6ef54620b10211d70db1e6326db
tanishp1 <pawartanish73@gmail.com>
2026-08-15 12:56:13 +0530

    event done 2

--- Event/index.js
+++ Event/index.js
@@ -1,35 +1,30 @@
-// let input = document.querySelector("input")
-// input.addEventListener("input", function(e){
-//     console.log(e.data)
-// })
+// let h1 = document.querySelector("h1");
 
-//  let h1 = document.querySelector("h1")
-//  h1.addEventListener("click",function(){
-//     h1.style.color = "red"
-//  })
-
-// let h1 = document.querySelector("h1")
-// h1.addEventListener("dblclick", function(){
-//     h1.style.color = "green"
-// })
-
-// let h1 = document.querySelector("h1")
-// function dblclick(){
-//     h1.style.color = "green"
-// }
-// h1.addEventListener("dblclick", dblclick)
-// h1.removeEventListener("dblclick", dblclick)
-
-// let input = document.querySelector("input")
-// input.addEventListener("input", function(e){
-//     if(e.data !== null){
-//         console.log(e.data)
+// window.addEventListener("keydown", function(e){
+//     if(e.key === " "){
+//         h1.textContent = "SPC";
+//     }
+//     else {
+//         h1.textContent = e.key;
 //     }
 // })
 
-let select = document.querySelector("select")
-let device = document.querySelector("#device")
-select.addEventListener("change", function(e){
-    // console.log(e.target.value)
-    device.textContent = `${e.target.value}`
+// let btn = document.querySelector("#btn")
+// let fileinp = document.querySelector("#fileinp")
+
+// btn.addEventListener("click", function(){
+//     fileinp.click()
+// })
+
+// fileinp.addEventListener("change", function(e){
+//     const file = e.target.files[0];
+//     if(file){
+//         btn.textContent = file.name;
+//     }
+// })
+
+let form = document.querySelector("form")
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    console.log("form submit")
 })
